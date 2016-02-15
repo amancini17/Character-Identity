c928d0ff545b028976e5bed6dc5ceef5488759d1
amancini17 <amancini17@deerfield.edu>
2016-02-15 17:50:00 -0500

    Imports

---
+++ CharacterIdentity.java
@@ -0,0 +1,20 @@
+
+import java.awt.BorderLayout; //imports stuff that's necessary for the code to work
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
